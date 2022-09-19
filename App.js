be827f773787c1dfd9432494ba2d89adf92e60f9
Partik SIngh <partiksingh@pop-os.localdomain>
2022-09-19 16:10:11 +0530

    Second

--- App.js
+++ App.js
@@ -1,14 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Player from './src/screens/Player';
 import AllPlaylist from './src/screens/AllPlaylist';
 import AllMusic from './src/screens/AllMusic';
 import Account from './src/screens/Account';
+import * as MediaLib from 'expo-media-library';
+import { useEffect, useState } from 'react';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [songs, setSongs] = useState({});
+  const permissionPopUp = async()=>{
+    Alert.alert("Permission Required","We need your permission to access your media library",[
+      {text:"Accept",onPress:()=>{MediaLib.requestPermissionsAsync();
+      getPermission();
+    }},
+    {text:"Cancel",onPress:()=>permissionPopUp()},
+  ]
+    )
+  }
+  // const getPermission =async()=>{
+  //   console.log("ugawugue");
+  //   const permission = await MediaLib.getPermissionsAsync();
+  //   console.log(permission);
+  //   if(permission.granted === false && permission.canAskAgain === true){
+  //     const newPermission = await MediaLib.requestPermissionsAsync();
+  //     if(newPermission.status === "denied"){
+  //       alert("Permission denied,Please allow permission to show your music");
+  //       MediaLib.requestPermissionsAsync();
+  //     }
+  //     if(newPermission.status === "denied" && newPermission.canAskAgain === false){
+  //       alert("Permission denied,Can't show your music");
+  //     }
+  //     if(newPermission.status === "granted"){
+  //       const {assets}= await MediaLib.getAssetsAsync();
+  //       console.log(assets);
+  //     }
+  //   }
+  //   if(permission.granted === true){
+  //     const {assets} = await MediaLib.getAssetsAsync();
+  //     console.log(assets);
+  //   }
+  // }
+  const getPermission = async () => {
+    const permission = await MediaLib.getPermissionsAsync();
+    console.log(permission);
+    if(permission.granted === false ){
+      const newPermission = await MediaLib.requestPermissionsAsync();
+      if(newPermission.status === "denied"){
+        // alert("Permission denied,Please allow permission to show your music");
+       permissionPopUp();
+      }
+      if(newPermission.status === "denied" && newPermission.canAskAgain === false){
+        // alert("Permission denied,Can't show your music");
+        // MediaLib.requestPermissionsAsync();
+        permissionPopUp();
+      }
+      if(newPermission.status === "granted"){
+        // const {assets}= await MediaLib.getAssetsAsync();
+        console.log("Permission granted showing your songs"); 
+        getAllSongs();
+        // console.log(assets);
+      }
+    }
+    else if(permission.granted === true){
+      // const {assets} = await MediaLib.getAssetsAsync();
+      // console.log(assets);
+      console.log("Permission granted showing your songs"); 
+      getAllSongs();
+    }
+  }
+  const getAllSongs = async()=>{
+    const {assets} = await MediaLib.getAssetsAsync({
+      mediaType:"audio",
+    });
+    console.log(assets);
+    setSongs(assets);
+    // console.log("Songs" , songs);
+  }
+  useEffect(() => {
+    getPermission();
+
+  }, [])
+  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Allmusic'>
